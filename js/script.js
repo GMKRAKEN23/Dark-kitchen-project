416d4c4049a2fe646f0dc6c16fe5ebe7c0278b25
root <root@DESKTOP-LCHE35F>
2023-12-20 13:58:32 +0100

    finition des boutons add to cart et détails

--- js/script.js
+++ js/script.js
@@ -18,70 +18,70 @@ const collection = [
         dish: "Pasta Bolognese",
         description: "A hearty pasta dish with a rich, slow-cooked meat sauce that typically includes ground beef, tomatoes, onions, and garlic.",
         category: "Category: Pasta",
-        ajouterpanier: "Ajouter au panier",
+        ajouterpanier: "Add to cart",
     },
     {
         picture: "assets/carbonara.jpeg",
         dish: "Pasta Carbonara",
         description: "A classic Italian dish featuring pasta served with a creamy sauce made from eggs, cheese, pancetta, and black pepper.",
         category: "Category: Pasta",
-        ajouterpanier: "Ajouter au panier",
+        ajouterpanier: "Add to cart",
     },
     {
         picture: "assets/pates_vege.jpeg",
         dish: "Vegetarian Pasta",
         description: "Vegetarian pasta dish with a medley of fresh vegetables, herbs, and possibly a flavorful tomato or cream sauce.",
         category: "Category: Pasta",
-        ajouterpanier: "Ajouter au panier",
+        ajouterpanier: "Add to cart",
     },
     {
         picture: "assets/filet_pur.jpeg",
         dish: "Filet Pur 250G",
         description: "A tender and juicy 250g beef tenderloin steak, often prepared to your preferred level of doneness.",
         category: "Category: Meat",
-        ajouterpanier: "Ajouter au panier",
+        ajouterpanier: "Add to cart",
     },
     {
         picture: "assets/Escal.jpg",
         dish: "Milanese Cutlet",
         description: "A Milanese-style dish featuring breaded and pan-fried veal or chicken cutlets, served with a wedge of lemon.",
         category: "Category: Meat",
-        ajouterpanier: "Ajouter au panier",
+        ajouterpanier: "Add to cart",
     },
     {
         picture: "assets/entrecote.jpeg",
         dish: "Ribeye Steak 350G",
         description: "A succulent 350g ribeye steak, known for its rich flavor and generous marbling.",
         category: "Category: Meat",
-        ajouterpanier: "Ajouter au panier",
+        ajouterpanier: "Add to cart",
     },
     {
         picture: "assets/Broch.jpg",
         dish: "Lamb Skewers",
         description: "Skewers of marinated and grilled lamb, offering a delightful combination of flavors and tenderness.",
         category: "Category: Meat",
-        ajouterpanier: "Ajouter au panier",
+        ajouterpanier: "Add to cart",
     },
     {
         picture: "assets/moules_mariniere.jpeg",
         dish: "Mussels Mariniere",
         description: "Fresh mussels cooked in a white wine, garlic, and herb broth, creating a savory and aromatic seafood dish.",
         category: "Category: Fish",
-        ajouterpanier: "Ajouter au panier",
+        ajouterpanier: "Add to cart",
     },
     {
         picture: "assets/sole_meuniere.webp",
         dish: "Sole Meuniere",
         description: "A classic French dish featuring sole fish dredged in flour and pan-fried in a buttery sauce with lemon and parsley.",
         category: "Category: Fish",
-        ajouterpanier: "Ajouter au panier",
+        ajouterpanier: "Add to cart",
     },
     {
         picture: "assets/dos_de_cabillaud.jpeg",
         dish: "Cod Fillet",
         description: "Grilled or baked cod fillet, known for its mild flavor and versatility, often served with complementary sauces or seasonings.",
         category: "Category: Fish",
-        ajouterpanier: "Ajouter au panier",
+        ajouterpanier: "Add to cart",
     },
 
 
@@ -138,7 +138,7 @@ collection.forEach(function (objet, index) {
 
     createCard(section, 'div', 'card__category', objet.category);
 
-    createCard(section, 'button', 'card__cart-icon', objet.ajouterpanier, 'Ajouter au panier');
+    createCard(section, 'button', 'card__ajouterpanier', objet.ajouterpanier, 'Ajouter au panier');
 
     if (index % 2 === 0) {
         section1.appendChild(section);
@@ -180,7 +180,7 @@ function filterProducts(category) {
         createCard(section, 'div', 'card__dish', objet.dish);
         createCard(section, 'div', 'card__description', objet.description);
         createCard(section, 'div', 'card__category', objet.category);
-        createCard(section, 'button', 'card__cart-icon', null, objet.ajouterpanier, 'Ajouter au panier');
+        createCard(section, 'button', 'ajouterpanier', null, objet.ajouterpanier, 'Ajouter au panier');
 
         if (index % 2 === 0) {
             section1.appendChild(section);
@@ -203,7 +203,7 @@ function resetFilter() {
         createCard(section, 'div', 'card__dish', objet.dish);
         createCard(section, 'div', 'card__description', objet.description);
         createCard(section, 'div', 'card__category', objet.category);
-        createCard(section, 'button', 'card__cart-icon', null, objet.ajouterpanier, 'Ajouter au panier');
+        createCard(section, 'button', 'ajouterpanier', null, objet.ajouterpanier, 'Ajouter au panier');
 
         if (index % 2 === 0) {
             section1.appendChild(section);
